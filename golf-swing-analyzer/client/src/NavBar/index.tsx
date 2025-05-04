9e4662ccea009c68736bc38cf8e734fca3eaa892
rampure4 <rampure@wisc.edu>
2025-05-04 13:21:27 -0500

    changed from white to black

--- golf-swing-analyzer/client/src/NavBar/index.tsx
+++ golf-swing-analyzer/client/src/NavBar/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div
-      className="d-flex flex-column p-4 bg-dark text-white"
+      className="d-flex flex-column p-4 bg-dark text-black"
       style={{
         width: "240px",
         height: "100vh",
